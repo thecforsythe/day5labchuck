600ce36620b7ad7d47f6806d124406a0f0b57da2
Chuck Forsythe <thecforsythe@gmail.com>
2017-06-10 13:16:28 -0500

    question2

--- app.js
+++ app.js
@@ -24,12 +24,15 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a,b){ //eslint-disable-line
-
+function multiply(a,b){
+  var timesTotal = (a * b);
+  return [timesTotal,'The product of ' + a + ' and ' + b + ' is ' + timesTotal + '.'];
 }
+console.log(sum(5,9));
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
